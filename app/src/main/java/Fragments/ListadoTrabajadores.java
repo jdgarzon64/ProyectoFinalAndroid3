a48835bc3196db65a943d5c8aa10fcc4ae072765
JuanDavid <jdgarzon64@misena.edu.co>
2018-05-02 16:35:32 -0500

    add components in the activities

---
+++ app/src/main/java/Fragments/ListadoTrabajadores.java
@@ -0,0 +1,31 @@
+package Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.unkwon.tallerencuestas.R;
+
+
+public class ListadoTrabajadores extends Fragment {
+
+    public ListadoTrabajadores() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_listado_trabajadores, container, false);
+    }
+}
